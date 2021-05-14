96cd83485299717928589321bef37b75d1c86eff
sunita369 <sunita98mondal@gmail.com>
2021-05-14 22:16:18 +0530

    Spring Security Assignment 1 Question 2 has been added

---
+++ Security 2/src/main/java/com/Security/sunita/Spring2Application.java
@@ -0,0 +1,13 @@
+package com.Security.sunita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2Application.class, args);
+	}
+
+}
