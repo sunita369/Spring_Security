365f40ce9f8aacc433ea3a4af8a0321273ef94b3
sunita369 <sunita98mondal@gmail.com>
2021-05-14 23:14:09 +0530

    Spring Security Assignment 2 question 2 has been added

---
+++ Spring Security 2/src/main/java/com/spring/security2/HelloController.java
@@ -0,0 +1,26 @@
+package com.spring.security2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/")
+	public String hello()
+	{	
+		return "Welcome";
+	}
+	@GetMapping("/user")
+	public String user()
+	{	
+		return "Welcome user";
+	}
+	@GetMapping("/admin")
+	public String admin()
+	{	
+		return "Welcome Admin";
+	}
+	
+
+}
---
+++ Spring Security 2/src/test/java/com/spring/security2and3/Security2and3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security2and3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security2and3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
